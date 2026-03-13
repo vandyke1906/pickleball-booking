94a88060888786ed79db9e002e50b54a0522d607
Ronie-MIADP <ronie.miadp@gmail.com>
2026-03-13 22:22:09 +0800

    debug bookings length

--- app/(public)/(components)/availability-court.tsx
+++ app/(public)/(components)/availability-court.tsx
@@ -23,7 +23,7 @@ const isBlockAvailableForCourt = (
   const proposedStartMin = toMinutes(proposedStart)
   const proposedEndMin = proposedStartMin + dur * 60
 
-  console.info({ proposedStartMin, proposedEndMin, court })
+  if (court.bookings.length) console.info({ proposedStartMin, proposedEndMin })
 
   return !court.bookings.some((b) => {
     const bookingStart = formatDateTime(b.startTime)
