7918347479c0dd325d6ea207009f4202798d1804
Ronie-MIADP <ronie.miadp@gmail.com>
2026-03-15 06:04:03 +0800

    removed background on login

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,8 @@
 "use client"
 
-import { BubbleBackground } from "@/components/animated/background/bubble-background"
-
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <BubbleBackground interactive={true} />
       <div className="relative z-10">{children}</div>
     </>
   )
