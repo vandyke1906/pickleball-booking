f9bd340f3b789d92042823ddf6d8da80164e33a6
Ronie-MIADP <ronie.miadp@gmail.com>
2026-03-31 19:13:09 +0800

    moved public page into server component

---
+++ app/(public)/(components)/organization-public-page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Features } from "@/app/(public)/(components)/features"
+import { Hero } from "@/app/(public)/(components)/hero"
+import FBMessengerChat from "@/app/(public)/(components)/fb-messenger-chat"
+import { useParams } from "next/navigation"
+import { Footer } from "@/app/(public)/(components)/footer"
+import { useEffect } from "react"
+
+export default function OrganizationPublicPage() {
+  const params = useParams()
+  const slugParam = params.slug ?? ""
+  const slug = Array.isArray(slugParam) ? slugParam[0] : (slugParam ?? "")
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }, [])
+
+  return (
+    <div className="relative z-10">
+      <Hero slug={slug} />
+      <Features />
+      <Footer />
+      <FBMessengerChat />
+    </div>
+  )
+}
